f1b9edfa7ab1453fda37a7e6a4a546d70f82586b
opiter09 <92452568+opiter09@users.noreply.github.com>
2023-06-21 16:51:43 -0400

    ncer vaguely works

--- toNitro.py
+++ toNitro.py
@@ -45,5 +45,31 @@ for val in sys.argv[1:]:
         new.write((0x18).to_bytes(4, "little"))
         new.write(file[16:])
         new.close()
+    elif (val.endswith(".vce") == True):
+        f = open(val, "rb")
+        file = f.read()
+        f.close()
+        new = open("test.ncer", "wb")
+        new.close()
+        new = open("test.ncer", "ab")        
         
+        offset = int.from_bytes(file[32:36], "little")
+        size = int((len(file) - offset) * 14/8)
+        new.write((0x5245434E).to_bytes(4, "big"))
+        new.write((0xFFFE0001).to_bytes(4, "big"))
+        new.write((16 + 32 + size).to_bytes(4, "little"))
+        new.write((0x10000100).to_bytes(4, "big"))
+        new.write((0x4B424543).to_bytes(4, "big"))
+        new.write((32 + size).to_bytes(4, "little"))
+        new.write(file[0x1C:0x1E])
+        new.write(bytes(2))
+        new.write((0x18).to_bytes(4, "little"))
+        new.write((4).to_bytes(4, "little"))
+        new.write(bytes(12))
+        for i in range(size // 14):
+            new.write((1).to_bytes(4, "little"))
+            new.write((i * 6).to_bytes(4, "little"))
+        for i in range(offset, len(file), 8):
+            new.write(file[i:(i + 6)])
+        new.close()
     
