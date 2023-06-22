a9dc6e8e18d8126c92046b1cbc17d9984c4f9a49
opiter09 <92452568+opiter09@users.noreply.github.com>
2023-06-22 11:44:17 -0400

    added screen

--- toNitro.py
+++ toNitro.py
@@ -1,6 +1,5 @@
 import os
 import sys
-import ctypes
 
 for val in sys.argv[1:]:
     if (val.endswith(".vcl") == True):
@@ -33,20 +32,21 @@ for val in sys.argv[1:]:
         new = open("test.ncgr", "ab")
 
         size = len(file) - 16
-        mod = ((size // 32) % 32) * 32
         new.write((0x5247434E).to_bytes(4, "big"))
         new.write((0xFFFE0101).to_bytes(4, "big"))
-        new.write((16 + 32 + size + mod).to_bytes(4, "little"))
+        new.write((16 + 32 + size).to_bytes(4, "little"))
         new.write((0x10000100).to_bytes(4, "big"))
         new.write((0x52414843).to_bytes(4, "big"))
-        new.write((32 + size + mod).to_bytes(4, "little"))
-        new.write(((size + mod) // 2048).to_bytes(2, "little"))
-        new.write((0x200003000000).to_bytes(6, "big"))
+        new.write((32 + size).to_bytes(4, "little"))
+        new.write((size // 2048).to_bytes(2, "little"))
+        if (file[4] == 0):
+            new.write((0x200003000000).to_bytes(6, "big"))
+        else:
+            new.write((0x200004000000).to_bytes(6, "big"))
         new.write(bytes(8))
-        new.write((size + mod).to_bytes(4, "little"))
+        new.write(size.to_bytes(4, "little"))
         new.write((0x18).to_bytes(4, "little"))
         new.write(file[16:])
-        new.write(bytes(mod))
         new.close()
     elif (val.endswith(".vce") == True):
         f = open(val, "rb")
@@ -85,3 +85,25 @@ for val in sys.argv[1:]:
             final = bin(big)[2:][0:-10] + ("0" * (10 - len(bin(mult)[2:]))) + bin(mult)[2:]
             new.write(int(final, 2).to_bytes(2, "little"))           
         new.close()
+    elif (val.endswith(".vsc") == True):
+        f = open(val, "rb")
+        file = f.read()
+        f.close()
+        new = open("test.nscr", "wb")
+        new.close()
+        new = open("test.nscr", "ab")
+
+        size = len(file) - 20
+        new.write((0x5243534E).to_bytes(4, "big"))
+        new.write((0xFFFE0001).to_bytes(4, "big"))
+        new.write((16 + 20 + size).to_bytes(4, "little"))
+        new.write((0x10000100).to_bytes(4, "big"))
+        new.write((0x4E524353).to_bytes(4, "big"))
+        new.write((20 + size).to_bytes(4, "little"))
+        new.write((int.from_bytes(file[12:16], "little") * 8).to_bytes(2, "little"))
+        new.write((int.from_bytes(file[16:20], "little") * 8).to_bytes(2, "little"))
+        new.write(bytes(4))
+        new.write((size).to_bytes(4, "little"))
+        new.write(file[20:])
+        new.close()
+        
